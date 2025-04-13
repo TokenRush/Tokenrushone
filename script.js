741f838d7f8fdcbc64c31d74b87592db32ee6b2e
TokenRush <abhisheksaptale568@gmail.com>
2025-04-13 12:13:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const input = document.getElementById('searchInput');
-const cards = document.querySelectorAll('.token-card');
 
-input.addEventListener('keyup', function () {
-  const filter = input.value.toLowerCase();
-  cards.forEach(card => {
-    const text = card.textContent.toLowerCase();
-    card.style.display = text.includes(filter) ? 'block' : 'none';
-  });
-});
